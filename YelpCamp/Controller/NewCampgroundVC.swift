eebcbe267dbc0a8bb0b0f780fa345efaacb3f2bd
Nathaniel Fargo <nathaniel.fargo@gmail.com>
2019-12-20 17:19:53 -0700

    Set up the nonfunctional newCampground View Controller

--- YelpCamp/Controller/NewCampgroundVC.swift
+++ YelpCamp/Controller/NewCampgroundVC.swift
@@ -19,7 +19,6 @@ class NewCampgroundVC: UIViewController {
     }
     
     
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -27,6 +26,6 @@ class NewCampgroundVC: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
